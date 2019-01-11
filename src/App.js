c7e656e5c96f4ed7bea74ed5c6320f5256f1d35c
Sergey Lebedev <oddyone@mail.ru>
2019-01-11 15:58:09 +0300

    Update Data in a List without Mutations.

--- src/App.js
+++ src/App.js
@@ -6,22 +6,16 @@ import {TodoForm, TodoList} from "./components/todo";
 import {addTodo, generateId} from "./lib/todoHelpers";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            todos: [
-                {id: 1, name: "Learn JSX", isComplete: true},
-                {id: 2, name: "Build an Awesome App", isComplete: false},
-                {id: 3, name: "Ship It!", isComplete: false}
-            ],
-            currentTodo: ""
-        };
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleEmptySubmit = this.handleEmptySubmit.bind(this);
-    }
+    state = {
+        todos: [
+            {id: 1, name: "Learn JSX", isComplete: true},
+            {id: 2, name: "Build an Awesome App", isComplete: false},
+            {id: 3, name: "Ship It!", isComplete: false}
+        ],
+        currentTodo: ""
+    };
 
-    handleSubmit(evt) {
+    handleSubmit = evt => {
         evt.preventDefault();
         const newId = generateId();
         const newTodo = {
@@ -35,20 +29,20 @@ class App extends Component {
             currentTodo: "",
             errorMessage: ""
         });
-    }
+    };
 
-    handleEmptySubmit(evt) {
+    handleEmptySubmit = evt => {
         evt.preventDefault();
         this.setState({
             errorMessage: "Please supply a todo name"
         });
-    }
+    };
 
-    handleInputChange(evt) {
+    handleInputChange = evt => {
         this.setState({
             currentTodo: evt.target.value
         });
-    }
+    };
 
     render() {
         const submitHandler = this.state.currentTodo
